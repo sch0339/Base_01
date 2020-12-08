f6aeebfe5a2937092b2fd9abe6099038edfebcd7
서 창현 <9816760@mail.com>
2020-12-08 10:30:49 +0900

    2

--- Kotlin-Udemy-Sample-01-base-init/app-java/src/main/java/tech/thdev/java_udemy_sample/view/main/MainActivity.java
+++ Kotlin-Udemy-Sample-01-base-init/app-java/src/main/java/tech/thdev/java_udemy_sample/view/main/MainActivity.java
@@ -4,14 +4,12 @@ import android.annotation.SuppressLint;
 import android.log.Log;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Arrays;
 
@@ -30,13 +28,11 @@ import tech.thdev.java_udemy_sample.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    @BindView(R.id.textView1)
+    TextView textView1;
 
-    private FloatingActionButton fab;
     long ttt = System.currentTimeMillis();
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,27 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // ButterKnife 사용
         ButterKnife.bind(this);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // static Util을 이용하여 replace 처리
-        // ActivityUtil
-        //         .replaceFragmentToActivity(
-        //                 getSupportFragmentManager(),
-        //                 MainFragment.getInstance(),
-        //                 R.id.frame_layout);
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-        // http://gaemi.github.io/rxjava/2016/12/02/async-task-with-rxjava.html
+        textView1.setText("test");
 
         /**
          * RXJava
@@ -106,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 //        Single01(); // Single
 //        Completable01(); // Completable
 
-
+        rxLifeCycle01(); //  안드로이드와 UI 라이프사이클을 대체한다기 보다 구독할 때 발생할 수 있는 메모리 누수를 방지하기 위해 사용합니다.
     }
 
     void observable00() {
@@ -286,5 +262,11 @@ public class MainActivity extends AppCompatActivity {
             currTime = System.currentTimeMillis();
         }
         return "delayTime=" + delayTime;
+    }
+
+    private void rxLifeCycle01() {
+//        Observable.just("hello rxworld")
+//                .compose(bindToLifecycle())
+//                .subscribe(s->toolbar.setTitle(s));
     }
 }
